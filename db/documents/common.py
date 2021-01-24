fb82b8f330fd11a49d14eaf1f92156a988af0a5c
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-01-24 19:52:41 +0100

    [UPDATE] missing uuid

--- db/documents/common.py
+++ db/documents/common.py
@@ -3,10 +3,11 @@ import uuid
 
 # created_at => from ObjectId (timestamp)
 class Common:
-    uuid = uuid.uuid4()
+    uuid = ""
     updated_at = ""
     shape = ""
 
     def __init__(self, updated_at, shape):
         self.updated_at = updated_at
         self.shape = shape
+        self.uuid = str(uuid.uuid4())
