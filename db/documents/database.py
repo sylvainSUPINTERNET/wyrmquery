2c3a91254aa577488ad232ed501e6367d4dc3e5c
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-01-26 22:30:31 +0100

    [UPDATE] metadata for draw

--- db/documents/database.py
+++ db/documents/database.py
@@ -1,5 +1,5 @@
 from .common import Common
-
+from metadata.TypesEnum import Types
 
 class Database(Common):
     name = ""
@@ -7,6 +7,7 @@ class Database(Common):
     position_y = 0
     width = 100
     height = 100
+    type = Types.DATABASE
 
     def __init__(self, updated_at, shape, name, position_x, position_y, width, height):
         super().__init__(updated_at, shape)
@@ -15,3 +16,4 @@ class Database(Common):
         self.position_y = position_y
         self.width = width
         self.height = height
+        self.type = Types.DATABASE.value
